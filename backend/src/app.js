348ecc282f5d3472d090624e4854d220855cbf7b
lautaroVal <valenzuelalautaro@yahoo.com>
2025-07-14 02:17:27 -0300

    Manejo de paquetes y comandos generales

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 
 const rutasTasks = require('./routes/tasks')
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 app.use(express.json());
 
 
--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const path = './src/data/tasks.json'
 
 const tasksController = {
     list: (req, res) => {
         try {
-            let tasks = fs.readFileSync('./src/data/tasks.json', 'utf-8');
-            let tasksJSON = JSON.parse(tasks);
+             const tasks = JSON.parse(fs.readFileSync(path, 'utf-8'))
             
             return res.status(200).json({
 				meta: {
@@ -13,7 +13,7 @@ const tasksController = {
 					status: 200
 				},
 				data: {
-					totalTasks: tasksJSON
+					totalTasks: tasks
 				}
 			})
 
@@ -37,14 +37,14 @@ const tasksController = {
                 createdAt: new Date()
             };
 
-            const tasks = JSON.parse(fs.readFileSync('./src/data/tasks.json', 'utf-8'))
+            const tasks = JSON.parse(fs.readFileSync(path, 'utf-8'))
             tasks.push(newTasks)
-            fs.writeFileSync('./src/data/tasks.json',JSON.stringify(tasks,null,2))
+            fs.writeFileSync(path, JSON.stringify(tasks,null,2))
             
-            return res.status(200).json({
+            return res.status(201).json({
 				meta: {
 					ok: true,
-					status: 200
+					status: 201
 				},
 				data: {
 					totalTasks: tasks,
@@ -61,10 +61,85 @@ const tasksController = {
         
     },
     updateTask: (req, res) => {
-        
+         try {
+            const {id} = req.params
+            const {title,description,completed} = req.body
+            const tasks = JSON.parse(fs.readFileSync(path, 'utf-8'))
+            
+            const index = tasks.findIndex(task => task.id === id)
+
+              if (index === -1) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Tarea no encontrada'
+            });
+        }
+
+            tasks[index] = {
+                ...tasks[index],
+                title: title ?? tasks[index].title,
+                description: description ?? tasks[index].description,
+                completed: typeof completed === 'boolean' ? completed : tasks[index].completed,
+                updatedAt: new Date()
+            }
+
+            fs.writeFileSync(path, JSON.stringify(tasks,null,2))
+
+            return res.status(200).json({
+				meta: {
+					ok: true,
+					status: 200,
+                    msg: 'Tarea actualizada',
+				},
+				data: {
+					taskUpdated: tasks[index],
+				}
+			})
+
+		} catch (error) {
+			console.log(error);
+			return res.status(error.status || 500).json({
+				ok: false,
+				msg: error.message || 'Comuníquese con el administrador del sitio'
+			})
+		}
     },
     removeTask: (req, res) => {
-        
+        try {
+            const {id} = req.params
+            const tasks = JSON.parse(fs.readFileSync(path, 'utf-8'))
+
+            const taskExists = tasks.some(task => task.id === id);
+            if (!taskExists) {
+                return res.status(404).json({
+                    ok: false,
+                    msg: 'Tarea no encontrada'
+                });
+            }
+
+            const updatedTasks = tasks.filter(task => task.id !== id)
+            
+            fs.writeFileSync(path, JSON.stringify(updatedTasks,null,2))
+
+            return res.status(200).json({
+				meta: {
+					ok: true,
+					status: 200,
+                    msg: 'Tarea eliminada',
+				},
+				data: {
+					tasks: updatedTasks,
+                    indexDeletedAt: id
+				}
+			})
+
+		} catch (error) {
+			console.log(error);
+			return res.status(error.status || 500).json({
+				ok: false,
+				msg: error.message || 'Comuníquese con el administrador del sitio'
+			})
+		}
     }
 }
 
